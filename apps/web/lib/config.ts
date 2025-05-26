f834556593837ebcbfe5df0883fa7eef57c79f62
Christo Todorov <chroxify@gmail.com>
2025-05-26 13:07:25 +0200

    fix: update config

--- apps/web/lib/config.ts
+++ apps/web/lib/config.ts
@@ -1,3 +1,6 @@
 import { jsonConfig } from "json.cf";
 
-export const config = jsonConfig({ id: "test" });
+export const config = jsonConfig({
+  id: "ulGcxbvPVlDXhz",
+  baseUrl: "http://localhost:8787/v1",
+});
